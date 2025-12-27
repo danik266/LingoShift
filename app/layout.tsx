0c1ac73b8c1762709d53db4b5014628184406c34
danik266 <aldiyar_08@bk.ru>
2025-12-27 17:32:34 +0500

    dsa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
-"use client"; // Важно!
-
+import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import { ReactLenis } from "lenis/react";
+import SmoothScrolling from "./components/SmoothScrolling"; // Импортируем ваш компонент
 
 const nunito = Nunito({ 
   subsets: ["latin", "cyrillic"],
   variable: '--font-nunito',
   weight: ['400', '500', '600', '700', '800']
 });
+
+// 👇 ВОТ ЗДЕСЬ УКАЗЫВАЕТСЯ НАЗВАНИЕ САЙТА
+export const metadata: Metadata = {
+  title: "LingoShift",
+  description: "Learn languages ​​in context.",
+  icons: {
+    icon: '/favicon.ico', 
+  }
+};
 
 export default function RootLayout({
   children,
@@ -18,9 +26,10 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${nunito.variable} font-nunito bg-[#f8fafc] text-[#1e293b] overflow-x-hidden antialiased`}>
-        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        {/* Оборачиваем контент в клиентский компонент */}
+        <SmoothScrolling>
           {children}
-        </ReactLenis>
+        </SmoothScrolling>
       </body>
     </html>
   );
